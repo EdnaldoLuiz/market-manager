8bca1a1da316dee0bf39e8d4bfafcaa332534bbd
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2023-12-22 18:57:24 -0300

    inital commit

---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,53 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+  
+  @Id
+  private Long id;
+  
+  private String nome;
+
+  private String email;
+
+  public Usuario() {
+
+  }
+
+  public Usuario(String nome, String email) {
+    this.nome = nome;
+    this.email = email;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  
+
+  
+  
+}
