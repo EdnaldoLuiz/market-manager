2f28401ec1493516fcba0515bc944a3dfb75844a
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-01-06 17:44:58 -0300

    feature: creating select effect on nav list

--- src/main/java/br/com/luiz/smktsystem/utils/javax/CustomColor.java
+++ src/main/java/br/com/luiz/smktsystem/utils/javax/CustomColor.java
@@ -7,5 +7,6 @@ public class CustomColor {
     public static final Color MAIN_RED = new Color(165, 39, 39);
     public static final Color SUCESS_GREEN = new Color(39, 165, 39);
     public static final Color DARK_GRAY = new Color(122, 138, 154);
+    public static final Color HOVER_RED = new Color(198, 43, 43);
     
 }
--- src/main/java/br/com/luiz/smktsystem/view/component/SidebarComponent.java
+++ src/main/java/br/com/luiz/smktsystem/view/component/SidebarComponent.java
@@ -2,43 +2,74 @@ package br.com.luiz.smktsystem.view.component;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.border.EmptyBorder;
 
 import br.com.luiz.smktsystem.utils.ResizeIcon;
+import br.com.luiz.smktsystem.utils.javax.CustomColor;
 
 public class SidebarComponent extends JPanel {
+
+    private JPanel selectedOption = null;
 
     public SidebarComponent() {
         setBackground(Color.RED);
         setPreferredSize(new Dimension(250, getHeight()));
-        setBorder(new EmptyBorder(40, 0, 0, 0)); 
+        setBorder(new EmptyBorder(40, 0, 0, 0));
 
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        add(createOption("Mercadorias", ResizeIcon.createResizedIcon("src/main/resources/icons/market.png", 30, 30)));
-        add(createOption("  Funcionarios", ResizeIcon.createResizedIcon("src/main/resources/icons/employee.png", 30, 30)));
+        JPanel mercadoriasOption = createOption("Mercadorias",
+                ResizeIcon.createResizedIcon("src/main/resources/icons/market.png", 30, 30));
+        add(mercadoriasOption);
+        selectOption(mercadoriasOption);
+
+        add(createOption("Funcionarios",
+                ResizeIcon.createResizedIcon("src/main/resources/icons/employee.png", 30, 30)));
+
         add(createOption("Estatisticas", ResizeIcon.createResizedIcon("src/main/resources/icons/info.png", 30, 30)));
+        add(createOption("Configuração", ResizeIcon.createResizedIcon("src/main/resources/icons/config.png", 30, 30)));
     }
 
     private JPanel createOption(String text, ImageIcon icon) {
         JPanel optionPanel = new JPanel();
         optionPanel.setBackground(Color.RED);
-        optionPanel.setBorder(new EmptyBorder(20, 10, 20, 10)); 
+        optionPanel.setBorder(new EmptyBorder(20, 10, 20, 10));
         optionPanel.setLayout(new BoxLayout(optionPanel, BoxLayout.X_AXIS));
-    
+
         JLabel textLabel = new JLabel(text);
         JLabel iconLabel = new JLabel(icon);
-    
+
         textLabel.setForeground(Color.WHITE);
-        textLabel.setFont(new Font("Arial", Font.BOLD, 22));
-    
+        textLabel.setFont(new Font("Arial", Font.PLAIN, 22));
+
         optionPanel.add(Box.createHorizontalGlue());
         optionPanel.add(textLabel);
-        optionPanel.add(Box.createRigidArea(new Dimension(10, 0))); 
+        optionPanel.add(Box.createRigidArea(new Dimension(10, 0)));
         optionPanel.add(iconLabel);
-        optionPanel.add(Box.createHorizontalGlue()); 
-    
+        optionPanel.add(Box.createHorizontalGlue());
+
+        optionPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                selectOption(optionPanel);
+            }
+        });
+
         return optionPanel;
     }
-    
+
+    private void selectOption(JPanel optionPanel) {
+        if (selectedOption != null) {
+            selectedOption.setBackground(Color.RED);
+            JLabel selectedLabel = (JLabel) selectedOption.getComponent(1);
+            selectedLabel.setForeground(Color.WHITE);
+            selectedLabel.setFont(new Font("Arial", Font.PLAIN, 22)); // Define a fonte como plano
+        }
+        selectedOption = optionPanel;
+        selectedOption.setBackground(CustomColor.HOVER_RED);
+        JLabel selectedLabel = (JLabel) selectedOption.getComponent(1);
+        selectedLabel.setFont(new Font("Arial", Font.BOLD, 22)); // Define a fonte como negrito
+    }
 }
